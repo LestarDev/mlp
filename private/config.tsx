dfce5b09b0ebee1c2c792875776664718dffbbaa
Lestardev <m.borkowski.2005@gmail.com>
2024-06-21 20:10:57 +0200

    add mainserver

---
+++ private/config.tsx
@@ -0,0 +1,17 @@
+const config = () => {
+    const serverPath = "private/backend/mainServer.js"
+
+    const host = "https://zsti.me/";
+    const user = "lestardev";
+    const password = "OptimusPrime9001!";
+    const dbName = "lestardev_sowka"
+
+
+    return ({
+        serverPath,
+        host, user, password, dbName
+    })
+
+}
+
+export default config
