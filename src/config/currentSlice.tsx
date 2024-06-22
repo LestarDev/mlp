18924330ad68cf6f375f25dcedebad65409b6245
Lestardev <m.borkowski.2005@gmail.com>
2024-06-22 06:51:21 +0200

    add more to reducer

--- src/config/currentSlice.tsx
+++ src/config/currentSlice.tsx
@@ -1,16 +1,16 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 import umiejetnosciType from "./types/umiejetnosciType";
 
-export type choosenType = 'Ekwipunek' | 'Umiejetnosci' | 'Zdolnosci' | 'Handouty';
+export type choosenType = 'Ekwipunek' | 'Umiejetnosci' | 'Zaklecia' | 'Handouty';
 
 
 export interface CurrentState {
     nick: string,
     lvl: number,
     dodHP: number,
-    Umysl: string,
-    Cialo: string,
-    Urok: string,
+    Umysl: number,
+    Cialo: number,
+    Urok: number,
     Szczescie: number,
     idUzytkownika: number,
     wybranyTyp: choosenType,
@@ -23,13 +23,13 @@ export interface CurrentState {
 
 export const initialState: CurrentState = {
     nick: "",
-    Cialo: '',
+    Cialo: 0,
     dodHP: 0,
     idUzytkownika: 0,
     lvl: 0,
     Szczescie: 0,
-    Umysl: '',
-    Urok: '',
+    Umysl: 0,
+    Urok: 0,
     wybranyTyp: 'Ekwipunek',
     refreshPage: false,
     exp: 0,
@@ -44,7 +44,33 @@ export const thisSlice = createSlice({
         setNick: (state, action: PayloadAction<string>) =>{
             state.nick=action.payload;
         },
-
+        setCialo: (state, action: PayloadAction<number>) => {
+            state.Cialo = action.payload;
+        },
+        setUmysl: (state, action: PayloadAction<number>) => {
+            state.Umysl = action.payload;
+        },
+        setUrok: (state, action: PayloadAction<number>) => {
+            state.Urok = action.payload;
+        },
+        setIdUzytkownika: (state, action: PayloadAction<number>) => {
+            state.idUzytkownika = action.payload;
+        },
+        setSzczescie: (state, action: PayloadAction<number>) => {
+            state.Szczescie = action.payload;
+        },
+        setLvl: (state, action: PayloadAction<number>) => {
+            state.lvl = action.payload;
+        },
+        setExp: (state, action: PayloadAction<number>) => {
+            state.exp = action.payload;
+        },
+        setZloteGalopy: (state, action: PayloadAction<number>) => {
+            state.zloteGalopy = action.payload;
+        },
+        setDodHP: (state, action: PayloadAction<number>) => {
+            state.dodHP = action.payload;
+        },
     }
 })
 
--- src/pages/MainPage.tsx/MainPage.tsx
+++ src/pages/MainPage.tsx/MainPage.tsx
@@ -5,12 +5,9 @@ import usePlayer from "../../hooks/usePlayer"
 
 const MainPage = ({loginOut}: pageType) => {
 
-    const player = usePlayer();
+    // const player = usePlayer();
 
     return <div>
-        <p>{player.getRangaOfUmiejka(1, true)}</p>
-        <p>{player.getRangaOfUmiejka(1)}</p>
-        <p>{player.getRangaOfUmiejka(3, true)}</p>
         <button onClick={loginOut}>Logout</button>
     </div>
 }
