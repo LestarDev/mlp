abcd1290de041a41c7fd50585924460c57bf44a9
Lestardev <m.borkowski.2005@gmail.com>
2024-07-23 17:12:32 +0200

    more stats css

--- src/components/CechaBox/CechaBox.tsx
+++ src/components/CechaBox/CechaBox.tsx
@@ -1,5 +1,6 @@
 import cechyType from "../../config/types/cechyType"
 import usePlayer from "../../hooks/usePlayer"
+import "./CechaBox.css"
 
 type cechaBoxType = {
     cecha: cechyType,
@@ -10,7 +11,7 @@ const CechaBox = ({cecha, value}: cechaBoxType) => {
     
     const player = usePlayer();
 
-    return <div>
+    return <div className="cechaBox">
         <span>{cecha}</span>
         <span>{player.recalculateToDices(value)}</span>
     </div>
