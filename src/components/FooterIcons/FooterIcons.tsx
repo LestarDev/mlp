81557019c0f54ac8cc8d814301808f37d3d95fa7
LestarDev <98614691+LestarDev@users.noreply.github.com>
2025-05-17 02:51:45 +0000

    add  more and more good content

--- src/components/FooterIcons/FooterIcons.tsx
+++ src/components/FooterIcons/FooterIcons.tsx
@@ -1,7 +1,23 @@
+import { BsBagHeart, BsSearchHeart } from "react-icons/bs"
+import { FaArrowCircleRight } from "react-icons/fa"
+import { PiListHeart } from "react-icons/pi"
+import { RiHomeHeartLine } from "react-icons/ri"
+
 const FooterIcons = () => {
     return <footer>
-        
+        <BsSearchHeart />
+        <PiListHeart />
+        <RiHomeHeartLine />
+        <BsBagHeart />
+        <FaArrowCircleRight />
     </footer>
 }
 
-export defaukt FooterIcons
+export default FooterIcons
+
+
+// BsSearchHeart
+// RiHomeHeartLine
+// PiListHeart
+// BsBagHeart
+// FaArrowCircleRight
